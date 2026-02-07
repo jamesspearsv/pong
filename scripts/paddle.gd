9dd9c19c5de1e4b592cf27d83a4569af09220aba
James Spears <jamesspearsv@gmail.com>
2026-02-07 17:37:12 -0500

    Initial commit

---
+++ scripts/paddle.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var speed = 400
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var movement = Vector2.ZERO
+	
+	# Set movement direction based on input
+	if (Input.is_action_pressed("ui_down")):
+		movement.y = 1
+	if (Input.is_action_pressed('ui_up')):
+		movement.y = -1
+		
+	position += movement * speed * delta
